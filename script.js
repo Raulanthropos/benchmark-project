22b2148bd98b6fa0d92dea31766b0c7bd7dca3c9
Raulopoulos <62166476+Raulopoulos@users.noreply.github.com>
2022-10-18 16:05:12 +0300

    added button functionality to switch pages

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const checkbox = document.getElementById("checkbox-play");
 const proceedButton = document.getElementById("proceed-button");
+const buttonLink = document.querySelector(".no-pointer");
 
 checkbox.addEventListener('change', function() {
     if (checkbox.checked) {
         proceedButton.disabled = false;
-        proceedButton.classList.remove("no-click")
+        proceedButton.classList.toggle("no-click");
+        buttonLink.classList.toggle("no-pointer")
     } else {
         proceedButton.disabled = true;
     }
