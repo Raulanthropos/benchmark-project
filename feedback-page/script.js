0617412d962828bcd22d9336afbccaaaffdb05b5
Raulopoulos <62166476+Raulopoulos@users.noreply.github.com>
2022-10-18 18:44:21 +0300

    added mouseleave event

--- feedback-page/script.js
+++ feedback-page/script.js
@@ -5,4 +5,8 @@ for (let i = 0; i < blackStar.length; i++) {
 blackStar[i].addEventListener('mouseover', function() {
     blackStar[i].classList.add("star-image");
 })
+
+blackStar[i].addEventListener('mouseleave', function() {
+    blackStar[i].classList.remove("star-image");
+})
 }
