85d0355f977195f22faeed1ca3d8ece9b583e5e6
Raulopoulos <62166476+Raulopoulos@users.noreply.github.com>
2022-10-18 14:14:29 +0300

    classlist remove

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const checkbox = document.getElementById("checkbox-play");
 const proceedButton = document.getElementById("proceed-button");
 
-checkbox.addEventListener('click', function(e) {
-    console.log(e.currentTarget.value);
-    const onEnable = e.currentTarget.value;
-    if (onEnable === "on") {
+checkbox.addEventListener('change', function() {
+    if (checkbox.checked) {
         proceedButton.disabled = false;
+        proceedButton.classList.remove("no-click")
     } else {
         proceedButton.disabled = true;
     }
