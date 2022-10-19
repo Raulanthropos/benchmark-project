9141a62e7e385924d0e24ef486ae7fa9e2679aeb
Raulopoulos <62166476+Raulopoulos@users.noreply.github.com>
2022-10-19 14:14:49 +0300

    stars functionality added as intended

--- feedback-page/script.js
+++ feedback-page/script.js
@@ -1,12 +1,38 @@
 const blackStar = document.querySelectorAll(".star-image-black");
-console.log(blackStar);
 
-for (let i = 0; i < blackStar.length; i++) {
-blackStar[i].addEventListener('mouseover', function() {
-    blackStar[i].classList.add("star-image");
-})
+// for (let i = 0; i < blackStar.length; i++) {
+//   blackStar[i].addEventListener("mouseover", function () {
+//     blackStar[i].classList.add("star-image");
+//   });
 
-blackStar[i].addEventListener('mouseleave', function() {
-    blackStar[i].classList.remove("star-image");
+//   blackStar[i].addEventListener("mouseleave", function () {
+//     blackStar[i].classList.remove("star-image");
+//   });
+// }
+
+blackStar.forEach((star, index) => {
+    star.addEventListener('mouseover', function() {
+        blackStar.forEach((otherStars, prevIndex) => {
+            if (prevIndex <= index) {
+                otherStars.classList.add("star-image");
+            }
+        })
+    })
+    star.addEventListener('mouseleave', function() {
+        blackStar.forEach((otherStars, prevIndex) => {
+            if (prevIndex <= index) {
+                otherStars.classList.remove("star-image");
+            }
+        })
+    })
+
+star.addEventListener('click', function() {
+        blackStar.forEach((otherStars, prevIndex) => {
+            if (prevIndex <= index) {
+                otherStars.classList.add("star-image-important");
+            } else {
+                otherStars.classList.remove("star-image-important");
+            }
+        })
+    })
 })
-}
